7c8c9c813f20ab6b09a2489ed3f99ef86103d9a5
ArchiXIII <archi.xiii@mail.ru>
2017-10-11 15:19:53 +0400

    Second variant of the solution. With java.util.concurrent.

--- src/main/java/ru/archi/Fork.java
+++ src/main/java/ru/archi/Fork.java
@@ -1,20 +1,28 @@
 package main.java.ru.archi;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Черный on 11.10.2017.
  */
 public class Fork {
     private boolean taken = false;
 
-    public synchronized boolean takeFork(){
+    private Lock locker = new ReentrantLock();
+
+    public boolean takeFork(){
+        locker.lock();
         if(!taken){
             taken = true;
             return true;
         }
+        locker.unlock();
         return false;
     }
 
-    public synchronized void putFork(){
+    public void putFork(){
         taken = false;
+        locker.unlock();
     }
 }
